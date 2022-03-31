4bd43a94f70edc6a6c1fe6288f05a00b92a56b5d
George Puryear <gm.puryear@yahoo.com>
2022-03-31 11:09:58 -0500

    Working on the add function

--- js/movies-project.js
+++ js/movies-project.js
@@ -1,4 +1,5 @@
 const url = "https://raspy-peaceful-alarm.glitch.me/movies";
+
 fetch(url)
     .then(r => r.json())
     .then(data => {
@@ -6,7 +7,8 @@ fetch(url)
         document.querySelector(".loader").style.display = "none"//stop the load
     })
 int()
-function int(){
+
+function int() {
     getData()
 }
 
@@ -32,11 +34,11 @@ function getData() {
 
             $('#cards').html(html)
 
-                $('.delete').click(function () {
-                    console.log("hello")
-                    console.log(this.id)
-                    deletemovie(this.id);
-                })
+            $('.delete').click(function () {
+                console.log("hello")
+                console.log(this.id)
+                deletemovie(this.id);
+            })
 
         })
         .catch(error => console.log(error))
@@ -44,12 +46,51 @@ function getData() {
 }
 
 function deletemovie(id) {
-    fetch(url + '/'+id, {
+    fetch(url + '/' + id, {
         method: 'DELETE',
         headers: {"Content-type": "application/json; charset=UTF-8"},
     })
         .then(res => res.json()) // or res.json()
         .then(res => getData())
         .catch(error => console.log('not deleted', error))
+};
+
+$('#sendNewMovie').click(function () {
+    console.log('hello');
+    let newMovie = {
+        title: document.querySelector('#movieTitle').value,
+        rating: document.querySelector('#rating').value,
+        poster: '../img/movie.jpg',
+        year: document.querySelector('#year').value,
+        genre: document.querySelector('#genre').value,
+        director: document.querySelector('#director').value,
+        plot: document.querySelector('#plot').value,
+        actors: document.querySelector('#actors').value,
+    }
+    // console.log(newMovie);
+    console.log(parseInt(newMovie.year));
+    if (!isNaN(Number(newMovie.year))) {
+        console.log('is a num');
+        test(newMovie);
+    }
+    else {
+        alert("Please enter a numerical year");
+    }
+})
+
+function test(testMovie) {
+    console.log(testMovie.year);
 }
+
+// function addCard() {
+//     html += `<div class="card" style="width: 18rem;">
+//   <img src="${movieData[i].poster}" class="card-img-top" alt="${movieData.title} movie poster" style="height: 200px; width: 150px">
+//     <div class="card-body">
+//         <h5 class="card-title">${.title}</h5>
+//     <p class="card-text">${movieData[i].plot}</p>
+//     <a href="#" class="btn btn-primary">Go somewhere</a>
+// </div>`
+// };
+
+
 
